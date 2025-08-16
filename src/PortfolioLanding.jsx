7302d6e5896458ad7f20687f234013d0c92c67a8
Therapthor <gusmelendezlopez@gmail.com>
2025-08-16 17:11:43 -0500

    Update PortfolioLanding.jsx

--- src/PortfolioLanding.jsx
+++ src/PortfolioLanding.jsx
@@ -12,7 +12,7 @@ import { motion, AnimatePresence } from "framer-motion";
    === CONFIG — EDIT THIS ONLY ===
    ============================= */
 const CONFIG = {
-  profilePhoto: "https://therapthor.github.io/Portafolio-Gustavo-Melendez/foto_perfil.JPG", // URL de la foto de perfil (debe estar en la carpeta pública)
+  profilePhoto: import.meta.env.BASE_URL + 'foto_perfil.JPG', // URL de la foto de perfil (debe estar en la carpeta pública)
   whatsappLink: "https://wa.me/51977430941",
 
   videos: [
